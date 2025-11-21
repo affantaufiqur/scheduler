7d5313298e4ebd64e394b3f5cc6e394f4581267a
affantaufiqur <affantaufiqur@gmail.com>
2025-11-21 16:33:32 +0700

    add drizzle.config.ts

---
+++ drizzle.config.ts
@@ -0,0 +1,24 @@
+import "dotenv/config";
+import { defineConfig } from "drizzle-kit";
+
+const URL = process.env.DATABASE_URL || "postgresql://postgres:password@localhost:5432/postgres";
+const HOST = process.env.DATABASE_HOST || "localhost";
+const PORT = Number(process.env.DATABASE_PORT!) || 5432;
+const USER = process.env.DATABASE_USER || "postgres";
+const PASSWORD = process.env.DATABASE_PASSWORD || "password";
+const DATABASE = process.env.DATABASE_NAME || "postgres";
+
+export default defineConfig({
+  out: "./drizzle",
+  schema: "./src/db/schema/",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: URL,
+    host: HOST,
+    port: PORT,
+    user: USER,
+    password: PASSWORD,
+    database: DATABASE,
+    ssl: false,
+  },
+});

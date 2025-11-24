6f0d9402acee6fceb039bd241a23d4defe02803b
affantaufiqur <affantaufiqur@gmail.com>
2025-11-24 11:52:16 +0700

    feat: logout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,20 @@
-import { Link } from "@tanstack/react-router";
+import { Link, redirect } from "@tanstack/react-router";
+import { createServerFn, useServerFn } from "@tanstack/react-start";
+import { LogOut } from "lucide-react";
+import { deleteCookie } from "@tanstack/react-start/server";
+
+const logout = createServerFn({ method: "POST" }).handler(async () => {
+  deleteCookie("token", {
+    path: "/",
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "lax",
+  });
+
+  throw redirect({
+    to: "/login",
+  });
+});
 
 interface HeaderProps {
   username: string;
@@ -18,6 +34,7 @@ const routes = [
 ];
 
 export default function Header({ username }: HeaderProps) {
+  const logoutFn = useServerFn(logout);
   return (
     <header className="border-b border-gray-200 bg-white">
       <nav className="">
@@ -36,6 +53,9 @@ export default function Header({ username }: HeaderProps) {
           </div>
           <div className="flex items-center">
             <span className="text-sm font-medium text-gray-700">{username}</span>
+            <button onClick={() => logoutFn()}>
+              <LogOut className="ml-4 h-5 w-5 cursor-pointer text-gray-500 hover:text-gray-700" />
+            </button>
           </div>
         </div>
       </nav>
